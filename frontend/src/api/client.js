1c97da096bb05aed85d82b3b7604c2171625c265
CarlosChasi13 <cachasi3@espe.edu.ec>
2025-08-05 12:16:25 -0500

    baseurl

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: '/api',
+  baseURL: 'https://analitycore-python-47k6.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000
 });
